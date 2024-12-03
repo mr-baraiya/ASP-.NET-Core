3ee24d6bfeaf8a2ec41c5e420d11939a7cd84433
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-12-03 21:15:42 +0530

    Add files via upload

---
+++ Lab-2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program-1
+            //Candidate c = new Candidate();
+            //c.getCandidateDetails();
+            //c.displayCandidateDetails();
+
+            //Progaram-2
+            //Staff[] staff = new Staff[5];
+            //for (int i = 0; i < staff.Length; i++) 
+            //{
+            //    staff[i] = new Staff();
+            //    Console.WriteLine("Get Details of " + (i+1) + "th Satff.");
+            //    staff[i].getStaffDetails();
+            //}
+            //for (int i = 0; i < staff.Length; i++)
+            //{
+            //    if (staff[i].designation.Equals("HOD"))
+            //    {
+            //        Console.WriteLine("Name : "+staff[i].name);
+            //        Console.WriteLine("Salary : "+staff[i].salary);
+            //        Console.WriteLine();
+            //    }
+            //}
+
+            //Program-3
+            //Bank_Account account = new Bank_Account();
+            //account.getAccountDetails();
+            //account.displayAccountDetails();
+
+            //Program-4
+            //Student student = new Student();
+            //student.displayStudentDetails();
+
+            //Program-5
+            //double length = 23.6;
+            //double width = 16.8;
+            //Rectangle r = new Rectangle(length, width);
+            //Console.WriteLine("Area of Rectangle  : " + r.calculateAreaOfRectangle());
+
+            //Program-6
+            //Interest i = new Interest();
+            //Console.WriteLine("Interest is : "+i.calculateIntrest());
+
+            //Program-7
+            //Console.WriteLine("Enter the basic pay of employee : ");
+            //int basic = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the TA pay of Employee : ");
+            //int ta = Convert.ToInt32(Console.ReadLine());
+            //Salary sa = new Salary(basic, ta);
+            //Console.WriteLine("Total Salary : "+sa.calSalary());
+
+            //Program-8
+            //Console.WriteLine("Enter the Disatnce1 : ");
+            //int dist1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the Distance2 : ");
+            //int dist2 = Convert.ToInt32(Console.ReadLine());
+            //Distance d = new Distance(dist1,dist2);
+            //d.addTwoDistance();
+            //d.displayAddition();
+
+            //Program-9
+            //String material = "wood";
+            //double price = 2345.56;
+            //double height = 100;
+            //double surface_area = 456;
+            //Table t = new Table(height, surface_area, material, price);
+
+            //Program-10
+
+        }
+    }
+}
---
+++ Lab-2/Staff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2.Write a program to create a class Staff having data members as Name,
+//Department, Designation, Experience & Salary. Accept this data for 5
+//different staffs and display only names & salary of those staff who are 
+//HOD.
+
+namespace ConsoleApp2
+{
+    internal class Staff
+    {
+        public String name;
+        public String department;
+        public String designation;
+        public int experience;
+        public double salary;
+
+        public void getStaffDetails()
+        {
+            Console.WriteLine("Enter The Name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the Department : ");
+            department = Console.ReadLine();
+            Console.WriteLine("Enter the Designation : ");
+            designation = Console.ReadLine();
+            Console.WriteLine("Enter the Experience : ");
+            experience = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Salary : ");
+            salary = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
