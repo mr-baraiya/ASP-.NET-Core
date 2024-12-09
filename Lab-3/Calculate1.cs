bf6e7854ca771cf4d994a1b1bd978bf9ab44c29f
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-12-09 01:32:58 -1200

    Add files via upload

---
+++ Lab-3/Calculate1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal interface Calculate1
+    {
+        int addition(int a, int b);
+        int substraction(int a, int b);
+    }
+
+    internal class Result : Calculate1
+    {
+        public int addition(int a,int b)
+        {
+            return a + b;
+        }
+        public int substraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
