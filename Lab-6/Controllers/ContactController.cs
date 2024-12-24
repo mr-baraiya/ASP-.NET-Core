aff5a9bc807ba0e03884c4965fe441b6a00e131a
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-12-24 19:27:30 +0530

    Add files via upload

---
+++ Lab-6/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project_DEMO.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
