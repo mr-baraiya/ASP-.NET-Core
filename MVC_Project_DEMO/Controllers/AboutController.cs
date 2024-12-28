d3771fbd1f2b1540c049a1f5a7ce0c3d51c8668d
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-12-28 16:06:33 +0530

    Add files via upload

---
+++ MVC_Project_DEMO/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project_DEMO.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Vishal()
+        {
+            return View();
+        }
+    }
+}
