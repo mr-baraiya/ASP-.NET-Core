26404cb600bf4cbf43016abff633187b55ec4952
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-01-21 22:46:44 +0530

    Add files via upload

---
+++ ThemeDesignDemo/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndThemes.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThemeDesignDemo/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndThemes.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Elements()
+        {
+            return View();
+        }
+    }
+}
