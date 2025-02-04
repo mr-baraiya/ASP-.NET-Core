af8b8b9df7697ec39d0679aefc5e8212e853e7e4
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-02-04 18:24:38 +0530

    Add files via upload

---
+++ AddressBook/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AddressBook.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+        public int Rating { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCompany { get; set; }
+    }
+}
