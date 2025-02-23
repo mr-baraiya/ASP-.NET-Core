8c198aee12e46c87374320c5c35f8df2d88fbfc0
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-02-23 18:57:56 +0530

    Add files via upload

---
+++ AddressBook/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+    public class StudentModel
+    {
+        //[Required(ErrorMessage ="Enter Student Name")]
+        //[Display()]
+        //[DisplayFormat()]
+        //[EmailAddress()]
+        //[CreditCard()]
+        //[RegularExpression()]
+        //[Range()]
+        //[StringLength()]
+        //[MinLength()]
+        //[MaxLength()]
+        //[Compare("Password")]
+        //[DataType(DataType.Password)]
+        //public string StudentName { get; set; }
+        
+    }
+}
---
+++ AddressBook/TagHelpers/VishalTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AddressBook.TagHelpers
+{
+    [HtmlTargetElement("vishal")]
+    public class VishalTagHelper : TagHelper
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Text { get; set; } = "Image"; // Default alt text
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+            output.TagName = "img"; // Change <vishal> to <img>
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("src", Name);
+            output.Attributes.SetAttribute("alt", Text);
+        }
+    }
+}
