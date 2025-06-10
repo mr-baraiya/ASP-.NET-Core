cf7fe688b0d40aa4902ae8fc5f1a01e13f4f9ea3
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-10 21:39:35 +0530

    Update CityCRUD

--- CityCRUD/Controllers/HomeController.cs
+++ CityCRUD/Controllers/HomeController.cs
@@ -1,22 +1,49 @@
-using System.Diagnostics;
 using CityCRUD.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace CityCRUD.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        #region configuration
+        public HomeController(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
         }
+        #endregion
 
+        #region Index
         public IActionResult Index()
         {
+            string connectionstr = this._configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connectionstr);
+
+            int cityCount = 0, stateCount = 0, countryCount = 0;
+            SqlCommand cmdCount = new SqlCommand("PR_LOC_GetCounts", conn);
+            cmdCount.CommandType = CommandType.StoredProcedure;
+            conn.Open();
+            SqlDataReader countReader = cmdCount.ExecuteReader();
+            if (countReader.Read())
+            {
+                countryCount = Convert.ToInt32(countReader["CountryCount"]);
+                stateCount = Convert.ToInt32(countReader["StateCount"]);
+                cityCount = Convert.ToInt32(countReader["CityCount"]);
+            }
+            conn.Close();
+
+            ViewBag.CountryCount = countryCount;
+            ViewBag.StateCount = stateCount;
+            ViewBag.CityCount = cityCount;
+
             return View();
         }
+        #endregion
 
         public IActionResult Privacy()
         {
