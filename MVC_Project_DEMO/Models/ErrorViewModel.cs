3b81b1c0bc37ffcec26d8de6ac793f4efbf8a281
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-10 12:48:43 +0530

    Delete MVC_Project_DEMO directory

--- MVC_Project_DEMO/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Project_DEMO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
