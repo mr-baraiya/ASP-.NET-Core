18482d2a7cf16813931269976fbe63c9570f9c0f
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-14 14:50:26 +0530

    Create User.cs

---
+++ ConsumeAPIDemoProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeAPIDemoProject.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string email { get; set; }
+        public DateTime createdAt { get; set; } = DateTime.Now;
+        public bool isActive { get; set; } = true;
+    }
+}
