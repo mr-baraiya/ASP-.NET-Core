1f5cc8be436c864dcce9ff5e42a5caf04d950be2
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-16 20:04:14 +0530

    Create Doctor.cs

---
+++ HospitalManagementProject/Models/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalManagementProject.Models;
+
+public partial class Doctor
+{
+    public int DoctorId { get; set; }
+
+    public string DoctorName { get; set; } = null!;
+
+    public string Degree { get; set; } = null!;
+
+    public int Experience { get; set; }
+
+    public string? Specification { get; set; }
+
+    public int Age { get; set; }
+
+    public DateTime Dob { get; set; }
+}
