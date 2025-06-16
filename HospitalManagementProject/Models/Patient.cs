41df4e77e178042babafa3911523f1fed422c51b
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-16 20:04:22 +0530

    Create Patient.cs

---
+++ HospitalManagementProject/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalManagementProject.Models;
+
+public partial class Patient
+{
+    public int PatientId { get; set; }
+
+    public string PatientName { get; set; } = null!;
+
+    public string Contact { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public bool EarlierOperation { get; set; }
+
+    public string BloodGroup { get; set; } = null!;
+}
