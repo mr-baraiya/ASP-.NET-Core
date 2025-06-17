e305668666885355424ed8965f83f7a1fb02b7ec
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-17 21:40:17 +0530

    Create JSONSerealizedAndDeSerealizedDemo

---
+++ JSONSerealizedAndDeSerealizedDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using JSONSerealizedAndDeSerealizedDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace JSONSerealizedAndDeSerealizedDemo.Controllers
+{
+    public class EmployeeController : ControllerBase
+    {
+        #region Serialized
+        [HttpPost("serialize")]
+        public IActionResult Create([FromBody] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var jsonOutput = JsonConvert.SerializeObject(employee, Formatting.Indented);
+                return Ok(new
+                {
+                    Message = "Employee data received and serialized successfully.",
+                    Data = jsonOutput
+                });
+            }
+            return BadRequest(ModelState);
+        }
+        #endregion
+
+        #region Deserialized
+        [HttpPost("deserialize")]
+        public IActionResult DeserializeJson([FromBody] string json)
+        {
+            try
+            {
+                var employee = JsonConvert.DeserializeObject<Employee>(json);
+
+                return Ok(new
+                {
+                    Message = "JSON deserialized successfully.",
+                    Employee = employee
+                });
+            }
+            catch (JsonException ex)
+            {
+                return BadRequest(new
+                {
+                    Message = "Invalid JSON format",
+                    Error = ex.Message
+                });
+            }
+        }
+        #endregion
+    }
+}
---
+++ JSONSerealizedAndDeSerealizedDemo/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace JSONSerealizedAndDeSerealizedDemo.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        [JsonRequired]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonIgnore]
+        public String PhoneNumber { get; set; }
+
+        public bool isActive { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
