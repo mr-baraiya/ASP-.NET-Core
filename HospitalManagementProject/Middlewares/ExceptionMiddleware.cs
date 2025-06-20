ebb02b4f56ab2ffefe18eb14be6f6bc9da3addd8
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-20 21:55:58 +0530

    Create ExceptionMiddleware.cs

---
+++ HospitalManagementProject/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HospitalManagementProject.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex) 
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsJsonAsync(JsonConvert.SerializeObject(new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+                status = "ERROR"
+            }));
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
