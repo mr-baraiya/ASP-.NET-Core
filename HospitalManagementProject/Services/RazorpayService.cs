a2649bfa4843a7f3aaa4ec8941dd7e285d10f995
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-08-07 11:45:24 +0530

    Integrate Razorpay payment service
    Added Razorpay NuGet package and created RazorpayService for order creation. Updated launchSettings.json to include Razorpay API credentials in environment variables for local development.

---
+++ HospitalManagementProject/Services/RazorpayService.cs
@@ -0,0 +1,23 @@
+﻿using Razorpay.Api;
+
+namespace HospitalManagementProject.Services
+{
+    public class RazorpayService
+    {
+        private readonly string key = "YOUR_KEY_ID";
+        private readonly string secret = "YOUR_KEY_SECRET";
+
+        public Order CreateOrder(int amount)
+        {
+            RazorpayClient client = new RazorpayClient(key, secret);
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options.Add("amount", amount * 100); // in paise
+            options.Add("currency", "INR");
+            options.Add("receipt", $"order_rcptid_{Guid.NewGuid()}");
+
+            Order order = client.Order.Create(options);
+            return order;
+        }
+    }
+}
+
