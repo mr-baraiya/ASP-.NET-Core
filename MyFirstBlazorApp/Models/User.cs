b8f295a08c7b4ffe970766aa2bbcb39bceb79ff3
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-02-26 21:50:32 +0530

    Update User.cs

--- MyFirstBlazorApp/Models/User.cs
+++ MyFirstBlazorApp/Models/User.cs
@@ -4,13 +4,16 @@ namespace MyFirstBlazorApp.Models
 {
     public class User
     {
-        [Required(ErrorMessage ="Full Name is Required")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Full Name must be between 5 and 25 characters")]
         public string? FullName { get; set; }
 
-        [Required(ErrorMessage = "Email is Required")]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string? Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Age is required")]
+        [Range(1, 100, ErrorMessage = "Age must be between 1 and 100.")]
         public int? Age { get; set; }
     }
 }
