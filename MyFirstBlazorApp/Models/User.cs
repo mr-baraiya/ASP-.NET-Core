cdabb9427fbd0630a472a06d5231910a3db6cddc
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-03-05 23:18:07 +0530

    Add user fields and validation
    Add password, confirm-password, phone, URL and credit-card fields to the user form and display section in FormComponent.razor, including ValidationMessage components and a form reset link. Update the User model to introduce validation for these new properties (password regex + Compare for confirm, Phone, Url, CreditCard) and replace the FullName StringLength attribute with explicit MinLength and MaxLength attributes to enforce bounds.

--- MyFirstBlazorApp/Models/User.cs
+++ MyFirstBlazorApp/Models/User.cs
@@ -5,7 +5,9 @@ namespace MyFirstBlazorApp.Models
     public class User
     {
         [Required(ErrorMessage = "Full Name is required")]
-        [StringLength(25, MinimumLength = 5, ErrorMessage = "Full Name must be between 5 and 25 characters")]
+        //[StringLength(25, MinimumLength = 5, ErrorMessage = "Full Name must be between 5 and 25 characters")]
+        [MinLength(5, ErrorMessage = "Full Name must be at least 5 characters")]
+        [MaxLength(25, ErrorMessage = "Full Name must be at most 25 characters")]
         public string? FullName { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
@@ -15,5 +17,26 @@ namespace MyFirstBlazorApp.Models
         [Required(ErrorMessage = "Age is required")]
         [Range(1, 100, ErrorMessage = "Age must be between 1 and 100.")]
         public int? Age { get; set; }
+
+        [Required(ErrorMessage ="Password is Required.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Phone(ErrorMessage = "Phone Number must be exactly 10 digits.")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Profile URL is required")]
+        [Url(ErrorMessage = "Invalid URL format")]
+        public string? Url { get; set; }
+
+        [Required(ErrorMessage = "Credit Card Number is required")]
+        [CreditCard(ErrorMessage ="Invalid Credit Card Number.")]
+        public string? CreditCardNumber { get; set; }
+
     }
 }
